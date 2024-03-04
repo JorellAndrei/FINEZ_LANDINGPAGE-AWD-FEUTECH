f1ead74228e42e4484a489bddcdf2309ce03a414
Jorell Andrei P. Finez <110623787+JorellAndrei@users.noreply.github.com>
2024-03-04 09:27:40 +0800

    Add files via upload

---
+++ 3 Background-Animation/Assets/JS/script.js
@@ -0,0 +1,6 @@
+for (let index = 0; index < 2000; index++) {
+    const element = document.createElement("span");
+    document.querySelector('.container').appendChild(element);
+    
+    document.body.style.background = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRcWnn3SPKIGux0njvKMiWeK94EiflW98L6A&usqp=CAU')";
+}
